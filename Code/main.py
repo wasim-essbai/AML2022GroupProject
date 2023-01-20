20365c8814de1163b2074ffb2b2a043add4ef5f4
unknown <w.essbai@studenti.unibg.it>
2023-01-20 18:28:43 +0100

    data and CNN set up

--- Code/main.py
+++ Code/main.py
@@ -1,1 +1,38 @@
-# main script to train the network
+import matplotlib
+
+matplotlib.use("Agg")
+
+from frC_net import FrCNet
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+from torch import nn
+import torch
+import time
+import utils
+
+# define training hyperparameters
+INIT_LR = 1e-3
+EPOCHS = 10
+# define the train and val splits
+TRAIN_SPLIT = 0.75
+VAL_SPLIT = 1 - TRAIN_SPLIT
+# set the device we will be using to train the model
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# load the dataset
+print("[INFO] loading the dataset...")
+full_dataset = utils.get_data()
+trainData, testData, valData = utils.get_data_split(full_dataset)
+
+# initialize the train, validation, and test data loaders
+trainDataLoader = DataLoader(trainData, shuffle=True,
+                             batch_size=utils.BATCH_SIZE)
+valDataLoader = DataLoader(valData, batch_size=utils.BATCH_SIZE)
+testDataLoader = DataLoader(testData, batch_size=utils.BATCH_SIZE)
+
+# calculate steps per epoch for training and validation set
+trainSteps = len(trainDataLoader.dataset) // utils.BATCH_SIZE
+valSteps = len(valDataLoader.dataset) // utils.BATCH_SIZE
+
+dataiter = iter(trainDataLoader)
+images, labels = dataiter.__next__()
---
+++ Code/utils.py
@@ -0,0 +1,42 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+from torch.utils.data import DataLoader
+
+# define training hyperparameters
+BATCH_SIZE = 40
+# define the train and val splits
+TRAIN_SPLIT = 0.70
+TEST_SPLIT = 0.15
+VAL_SPLIT = 1 - TRAIN_SPLIT - TEST_SPLIT
+
+
+def get_data():
+    data_dir = './function generation/generated_dataset'
+
+    transform = transforms.Compose([
+        # transforms.RandomRotation(20),
+        transforms.RandomResizedCrop(128),
+        # transforms.RandomHorizontalFlip(),
+        transforms.ToTensor()])
+
+    full_dataset = datasets.ImageFolder(data_dir, transform=transform)
+
+    full_data = DataLoader(full_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    return full_data
+
+
+def get_data_split(full_dataset):
+    # calculate the train/validation split
+    print("[INFO] generating the train/test/validation split...")
+    print("Total samples number: ", len(full_dataset))
+    numTrainSamples = int(len(full_dataset) * TRAIN_SPLIT)
+    numTestSamples = int(len(full_dataset) * TEST_SPLIT)
+    numValSamples = int(len(full_dataset) * VAL_SPLIT)
+    (trainData, testData, valData) = random_split(full_dataset,
+                                                  [numTrainSamples, numTestSamples, numValSamples],
+                                                  generator=torch.Generator().manual_seed(42))
+
+    return trainData, testData, valData
